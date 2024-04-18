d36a725401fdd90a95230400b8989c2dcdd2c05b
Owen Tsai <owentsai.v@gmail.com>
2024-04-18 16:19:49 +0800

    refactor: use `type-fest` to type the `bodyCell` argument

--- src/types/common.d.ts
+++ src/types/common.d.ts
@@ -2,3 +2,15 @@ type PaginatedList<T> = {
   total: number
   list: Array<T>
 }
+
+type TableScope<T> =
+  | {
+      column: import('ant-design-vue').TableColumnType<T> & {
+        key?: import('type-fest').LiteralUnion<keyof T, string> | number
+      }
+      record: T
+      index: number
+      text: T<keyof T>
+      value: any
+    }
+  | undefined
