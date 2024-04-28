1c3b754348778b26993430fe3ec94538cf8763cf
Owen Tsai <owentsai.v@gmail.com>
2024-04-28 17:07:17 +0800

    style: prettier

---
+++ src/api/infra/api-log/access-log.ts
@@ -0,0 +1,40 @@
+import request from '@/utils/request'
+
+export type AccessLogVO = {
+  id: number
+  traceId: string
+  userId: number
+  userType: number
+  applicationName: string
+  requestMethod: string
+  requestParams: string
+  requestUrl: string
+  userIp: string
+  userAgent: string
+  beginTime: Date
+  endTime: Date
+  duration: number
+  resultCode: number
+  resultMsg: string
+  createTime: Date
+}
+
+export type ListQueryParams = CommonQueryParams & {
+  userId?: number
+  applicationName?: string
+  requestMethod?: string
+  resultCode?: string
+  requestUrl?: string
+  beginTime?: [string, string]
+}
+
+export const getList = (params?: ListQueryParams) => {
+  return request.get<PaginatedList<AccessLogVO>>({
+    url: '/infra/api-access-log/page',
+    params
+  })
+}
+
+export const exportLog = (params?: ListQueryParams) => {
+  return request.download('/system/api-access-log/export-excel', params, 'API访问日志')
+}
