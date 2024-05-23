cf430586d5c13d4596d40070258b9fd459e61659
Owen Tsai <owentsai.v@gmail.com>
2024-05-23 14:00:56 +0800

    feat(infra/job): job log

---
+++ src/api/infra/job/log.ts
@@ -0,0 +1,33 @@
+import request from '@/utils/request'
+
+export type JobLogVO = {
+  jobId?: number
+  handlerName?: string
+  handlerParam?: string
+  executeIndex?: number
+  beginTime?: number
+  endTime?: number
+  duration?: number
+  status?: number
+  result?: string
+  id?: number
+  createTime?: number
+}
+
+export type ListQueryParams = CommonQueryParams & {
+  handlerName?: string
+  beginTime?: string
+  endTime?: string
+  status?: number
+}
+
+export const getList = (params?: ListQueryParams) => {
+  return request.get<PaginatedList<JobLogVO>>({
+    url: '/infra/job-log/page',
+    params
+  })
+}
+
+export const exportLog = (params?: ListQueryParams) => {
+  return request.download('/system/job-log/export-excel', params, '定时任务执行日志')
+}
