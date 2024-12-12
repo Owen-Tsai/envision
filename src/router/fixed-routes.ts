9ffebec335e497baa69646187907ddfc9ecb040b
张宇 <1395561508@qq.com>
2024-12-12 14:39:52 +0800

    1、删除误上传的demo路由

--- src/router/fixed-routes.ts
+++ src/router/fixed-routes.ts
@@ -90,14 +90,6 @@ export const fixedRoutes: RouteRecordRaw[] = [
         meta: {
           title: '应用审核'
         }
-      },
-      {
-        path: '/_demo',
-        name: 'DEMO',
-        component: () => import('@/views/_demo/index.vue'),
-        meta: {
-          title: '测试页面'
-        }
       }
     ]
   }
