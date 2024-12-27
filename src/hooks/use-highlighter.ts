bfa622b26c862525a73c700400b03eb522359624
Owen Tsai <owentsai.v@gmail.com>
2024-12-27 13:35:12 +0800

    fix: prevent `use-highlighter` from warning about undefined input

--- src/hooks/use-highlighter.ts
+++ src/hooks/use-highlighter.ts
@@ -3,6 +3,8 @@ import useAppStore from '@/stores/app'
 import type { BundledLanguage } from 'shiki'
 
 const useHighlighter = (code: string, lang: BundledLanguage): string => {
+  if (!code || code.trim() === '') return code
+
   const { theme } = useAppStore()
 
   const ret = highlighter.codeToHtml(code, {
