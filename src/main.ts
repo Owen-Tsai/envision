d915c50f22192d12436aff1e26f3ad00a72c08e1
Owen Tsai <owentsai.v@gmail.com>
2025-01-09 16:11:21 +0800

    refactor: entry file

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,7 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-import Antd from 'ant-design-vue'
-import Scrollbar from 'custom-vue-scrollbar'
-import DictTag from '@/components/dict-tag/index.vue'
-import Upload from '@/components/upload/index.vue'
-import Logo from '@/components/_internal/logo.vue'
-import Counter from 'vue-countup-v3'
-import STable from '@surely-vue/table'
-import { iconPlugin } from '@fusionx/utils'
-import '@/utils/dayjs'
-import '@/utils/echarts'
-// use all the icons
-import * as antIcons from '@ant-design/icons-vue'
-import 'ant-design-vue/dist/reset.css'
-import 'nprogress/nprogress.css'
-import 'custom-vue-scrollbar/dist/style.css'
+import registerGlobalComponents from '@/plugins/global-components'
+import registerAntd from '@/plugins/ant-design'
+
 import 'virtual:uno.css'
-import '@wangeditor/editor/dist/css/style.css'
 
 import '@/styles/index.scss'
 
@@ -26,19 +11,8 @@ import './router/guard'
 
 const app = createApp(App)
 
+registerGlobalComponents(app)
+registerAntd(app)
 app.use(createPinia())
-app.use(Antd)
 app.use(router)
-app.use(STable)
-
-app.component('Scrollbar', Scrollbar)
-app.component('EDictTag', DictTag)
-app.component('EUpload', Upload)
-app.component('ECounter', Counter)
-app.component('Logo', Logo)
-
-app.use(iconPlugin, {
-  icons: antIcons,
-})
-
 app.mount('#app')
