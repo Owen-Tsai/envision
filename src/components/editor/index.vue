c9175411d33a6c89162b1dd09aa5a559ec6e740d
张宇 <1395561508@qq.com>
2025-04-03 16:49:21 +0800

    1、引入富文本编辑器tinymce并适配，替换已停止维护的wangeditor

--- src/components/editor/index.vue
+++ src/components/editor/index.vue
@@ -1,38 +1,68 @@
 <template>
-  <div
-    :class="[
-      'editor-wrapper',
-      'h-full',
-      'overflow-y-auto',
-      {
-        'document-mode': props.type === 'document',
-      },
-    ]"
-  >
-    <Toolbar :editor="editorRef" :default-config="toolbarConfig" class="toolbar" mode="default" />
-    <div class="editor-body" @click="onBodyClick">
-      <Editor
-        v-model="content"
-        :default-config="editorConfig"
-        style="min-height: 350px"
-        mode="default"
-        @on-created="onCreated"
-      />
-    </div>
+  <div>
+    <Editor id="tinymceEditor" v-model="content" :init="init"></Editor>
   </div>
 </template>
 
 <script lang="ts" setup>
-import { ref, shallowRef, computed, onBeforeUnmount, onMounted, type PropType } from 'vue'
-import { message } from 'ant-design-vue'
-import { Toolbar, Editor } from '@wangeditor/editor-for-vue'
-import { getTenantId, getFormattedToken } from '@/utils/auth'
-import type { IEditorConfig, IToolbarConfig } from '@wangeditor/editor'
+import tinymce from 'tinymce/tinymce' //tinymce核心文件
+import Editor from '@tinymce/tinymce-vue'
+import 'tinymce/models/dom' // 引入dom模块。从 Tinymce6，开始必须有此模块导入
+import 'tinymce/skins/ui/oxide/skin.css' //样式
+import 'tinymce/themes/silver' //默认主题
+import 'tinymce/icons/default' //引入编辑器图标icon，不引入则不显示对应图标
+import 'tinymce/models/dom' // 引入dom模块。从 Tinymce6，开始必须有此模块导入
+import 'tinymce/themes/silver' //默认主题
+import 'tinymce/icons/default' //引入编辑器图标icon，不引入则不显示对应图标
+import 'tinymce/plugins/advlist' //高级列表
+import 'tinymce/plugins/anchor' //锚点
+import 'tinymce/plugins/autolink' //自动链接
+import 'tinymce/plugins/autoresize' //编辑器高度自适应,注：plugins里引入此插件时，Init里设置的height将失效
+import 'tinymce/plugins/autosave' //自动存稿
+import 'tinymce/plugins/charmap' //特殊字符
+import 'tinymce/plugins/code' //编辑源码
+import 'tinymce/plugins/codesample' //代码示例
+import 'tinymce/plugins/directionality' //文字方向
+import 'tinymce/plugins/emoticons' //表情
+import 'tinymce/plugins/fullscreen' //全屏
+import 'tinymce/plugins/help' //帮助
+import 'tinymce/plugins/image' //插入编辑图片
+import 'tinymce/plugins/importcss' //引入css
+import 'tinymce/plugins/insertdatetime' //插入日期时间
+import 'tinymce/plugins/link' //超链接
+import 'tinymce/plugins/lists' //列表插件
+import 'tinymce/plugins/media' //插入编辑媒体
+import 'tinymce/plugins/nonbreaking' //插入不间断空格
+import 'tinymce/plugins/pagebreak' //插入分页符
+import 'tinymce/plugins/preview' //预览
+import 'tinymce/plugins/quickbars' //快速工具栏
+import 'tinymce/plugins/save' //保存
+import 'tinymce/plugins/searchreplace' //查找替换
+import 'tinymce/plugins/table' //表格
+// import 'tinymce/plugins/template'; //内容模板
+import 'tinymce/plugins/visualblocks' //显示元素范围
+import 'tinymce/plugins/visualchars' //显示不可见字符
+import 'tinymce/plugins/wordcount' //字数统计
 
-type InsertFnType = (url: string, alt: string, href: string) => void
+const init = {
+  selector: '#tinymceEditor',
+  language_url: '/public/tinymce/langs/zh_CN.js',
+  language: 'zh_CN',
+  promotion: false, //Upgrade是否开启
+  branding: false, //tiny技术支持信息是否显示
+  plugins: `codesample preview searchreplace autolink directionality
+  visualblocks visualchars image link media template table
+  charmap pagebreak nonbreaking anchor insertdatetime advlist
+  lists wordcount autosave`, //引入工具插件
+  toolbar: `preview codesample code forecolor backcolor styles directionality fontfamily fontsize searchreplace image  media link alignleft aligncenter alignright template table charmap pagebreak nonbreaking anchor
+  insertdatetime advlist lists wordcount autosave `, //工具栏显示
+  line_height_formats: '1 1.2 1.4 1.6 2', //行高
+  font_size_formats: '12px 14px 16px 18px 20px 22px 24px 28px 32px 36px 48px 56px 72px', //字体大小
+  font_family_formats:
+    '微软雅黑=Microsoft YaHei,Helvetica Neue,PingFang SC,sans-serif;苹果苹方=PingFang SC,Microsoft YaHei,sans-serif;宋体=simsun,serif;仿宋体=FangSong,serif;黑体=SimHei,sans-serif;Arial=arial,helvetica,sans-serif;Arial Black=arial black,avant garde;Book Antiqua=book antiqua,palatino;',
+}
 
-const editorRef = shallowRef()
-const toolbar = ref()
+tinymce.init({})
 
 const props = defineProps({
   type: {
@@ -52,107 +82,4 @@ const content = computed({
     emit('update:value', val)
   },
 })
-
-const toolbarConfig: Partial<IToolbarConfig> = {
-  excludeKeys: ['emotion', 'fullScreen'],
-}
-const editorConfig = computed<Partial<IEditorConfig>>(() => {
-  return {
-    placeholder: '请输入内容...',
-    scroll: true,
-    autoFocus: false,
-    customAlert(info, type) {
-      switch (type) {
-        case 'success':
-          message.success(info)
-          break
-        case 'info':
-          message.info(info)
-          break
-        case 'warning':
-          message.warning(info)
-          break
-        case 'error':
-          message.error(info)
-          break
-        default:
-          message.info(info)
-          break
-      }
-    },
-    MENU_CONF: {
-      ['uploadImage']: {
-        server: import.meta.env.VITE_UPLOAD_URL,
-        maxFileSize: (10 * 1024) & 1024,
-        maxNumberOfFiles: 20,
-        // 自定义上传参数，例如传递验证的 token 等。参数会被添加到 formData 中，一起上传到服务端
-        meta: { updateSupport: 0 },
-        // 将 meta 拼接到 url 参数中，默认 false
-        metaWithUrl: true,
-        // custom http request headers
-        headers: {
-          Accept: '*',
-          Authorization: getFormattedToken(),
-          'tenant-id': getTenantId(),
-        },
-        // 跨域是否传递 cookie ，默认为 false
-        withCredentials: true,
-        // 超时时间，默认为 10 秒
-        timeout: 5 * 1000, // 5 秒
-        // form-data fieldName，后端接口参数名称，默认值wangeditor-uploaded-image
-        fieldName: 'file',
-        onFailed(file: File, res: any) {
-          alert(res.message)
-          console.log('onFailed', file, res)
-        },
-        onError(file: File, err: any, res: any) {
-          alert(err.message)
-          console.error('onError', file, err, res)
-        },
-        // 自定义插入图片
-        customInsert(res: any, insertFn: InsertFnType) {
-          insertFn(res.data, 'image', res.data)
-        },
-      },
-    },
-    uploadImgShowBase64: true,
-  }
-})
-
-const onCreated = (editor: unknown) => {
-  editorRef.value = editor
-}
-
-const onBodyClick = () => {
-  editorRef.value.focus()
-}
-
-onBeforeUnmount(() => {
-  editorRef.value.destroy()
-})
-
-onMounted(() => {
-  console.log(toolbar.value)
-})
 </script>
-
-<style lang="scss" scoped>
-.editor-wrapper {
-  --w-e-toolbar-border-color: var(--color-border);
-  border: 1px solid var(--color-border);
-  @apply relative;
-  .toolbar {
-    @apply sticky top-0 z-999;
-    border-bottom: 1px solid var(--color-border);
-  }
-
-  &.document-mode {
-    .editor-body {
-      width: 850px;
-      padding: 24px 32px;
-      margin: 32px auto;
-      background-color: var(--color-bg-container);
-    }
-  }
-}
-</style>
